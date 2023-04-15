c6093306951f3adef5cb93302e7c7aa314a8ac0a
Ruzanov Alexander <ru.booking@yandex.ru>
2023-04-15 14:27:40 +0400

    add phonebook

---
+++ src/Phonebook/Client/Program.java
@@ -0,0 +1,14 @@
+package Phonebook.Client;
+
+import Phonebook.Core.MVP.View;
+import Phonebook.UI.App;
+import Phonebook.UI.ConsoleView;
+
+public class Program {
+
+    public static void main(String[] args) {
+        
+        App.ButtonClick();
+
+    }
+}
---
+++ src/Phonebook/Core/MVP/View.java
@@ -0,0 +1,17 @@
+package Phonebook.Core.MVP;
+
+public interface View {
+    String getFirstName();
+    void setFirstName(String value);
+    String getLastName();
+    void setLastName(String value);
+    String getDescription();
+    void setDescription(String value);
+    String getCompany();
+    void setCompany(String value);
+    String getPhone();
+    void setPhone(String value);
+
+
+
+}
