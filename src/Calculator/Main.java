44acbca90258ee9b03b6d494d001db898a2d38ef
Alex Ur <ru.booking@yandex.ru>
2023-04-18 20:44:18 +0300

    fix Main

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -1,5 +1,7 @@
 package Calculator;
 
+import java.text.DecimalFormat;
+
 public class Main {
     public static void main(String[] args) {
         CalculateComplex calculateComplex = new CalculateComplex(15, 37);
@@ -8,7 +10,12 @@ public class Main {
         System.out.println("sum = " + calculateComplex.sum());
         System.out.println("diff = " + calculateComplex.diff());
         System.out.println("multi = " + calculateComplex.multi());
-        System.out.printf("div= " + calculateComplex.div());
+        double resulty = calculateComplex.div().getY();
+        Double resultx = calculateComplex.div().getX();
+        String formattedDoublex = new DecimalFormat("#0.00").format(resulty);
+        String formattedDoubley = new DecimalFormat("#0.00").format(resultx);
+
+        System.out.printf("div= " + formattedDoublex + " " + formattedDoubley);
 
     }
 }
